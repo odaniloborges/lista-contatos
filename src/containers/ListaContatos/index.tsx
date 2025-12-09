e898977f1ba25c4e840cbbf465f0f3f3b9742e4a
Danilo Borges <daniloborges.dev@gmail.com>
2025-12-09 19:05:41 -0300

    adicionei atributos ao componente de contatos

--- src/containers/ListaContatos/index.tsx
+++ src/containers/ListaContatos/index.tsx
@@ -1,14 +1,43 @@
 import Contato from '../../components/Contato'
+
 import { MainContainer, Titulo } from '../../styles'
+
+const contatos = [
+  {
+    id: 1,
+    nome: 'Jai Barcellos',
+    telefone: '+119543432541',
+    email: 'jai@gmail.com'
+  },
+  {
+    id: 2,
+    nome: 'Jai Barcellos',
+    telefone: '+119543432541',
+    email: 'jai@gmail.com'
+  },
+  {
+    id: 3,
+    nome: 'Jai Barcellos',
+    telefone: '+119543432541',
+    email: 'jai@gmail.com'
+  }
+]
 
 const ListaContatos = () => {
   return (
     <MainContainer>
       <Titulo as="p">Lista de Contatos</Titulo>
       <ul>
-        <li>
-          <Contato />
-        </li>
+        {contatos.map((c) => (
+          <li key={c.nome}>
+            <Contato
+              id={c.id}
+              nome={c.nome}
+              telefone={c.telefone}
+              email={c.email}
+            />
+          </li>
+        ))}
       </ul>
     </MainContainer>
   )
